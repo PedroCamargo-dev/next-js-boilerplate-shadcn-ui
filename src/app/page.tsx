06a6bc3cb5e1f3d90296c8bb7a3270e1ef717e1b
Pedro Camargo <pedrocamargo3008@gmail.com>
2025-06-16 19:40:20 -0300

    chore: update Tailwind CSS and PostCSS configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ import { ModeToggle } from '@/components/theme'
 export default function Home() {
   return (
     <>
-      <div className="absolute right-4 top-4">
+      <div className="absolute top-4 right-4">
         <ModeToggle />
       </div>
-      <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
+      <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-(family-name:--font-geist-sans) sm:p-20">
         <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
           <Image
             className="dark:invert"
@@ -17,10 +17,10 @@ export default function Home() {
             height={38}
             priority
           />
-          <ol className="list-inside list-decimal text-center font-[family-name:var(--font-geist-mono)] text-sm sm:text-left">
+          <ol className="list-inside list-decimal text-center font-(family-name:--font-geist-mono) text-sm sm:text-left">
             <li className="mb-2">
               Get started by editing{' '}
-              <code className="rounded bg-black/[.05] px-1 py-0.5 font-semibold dark:bg-white/[.06]">
+              <code className="rounded bg-black/5 px-1 py-0.5 font-semibold dark:bg-white/6">
                 src/app/page.tsx
               </code>
               .
@@ -30,7 +30,7 @@ export default function Home() {
 
           <div className="flex flex-col items-center gap-4 sm:flex-row">
             <a
-              className="flex h-10 items-center justify-center gap-2 rounded-full border border-solid border-transparent bg-foreground px-4 text-sm text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] sm:h-12 sm:px-5 sm:text-base"
+              className="bg-foreground text-background flex h-10 items-center justify-center gap-2 rounded-full border border-solid border-transparent px-4 text-sm transition-colors hover:bg-[#383838] sm:h-12 sm:px-5 sm:text-base dark:hover:bg-[#ccc]"
               href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               target="_blank"
               rel="noopener noreferrer"
@@ -45,7 +45,7 @@ export default function Home() {
               Deploy now
             </a>
             <a
-              className="flex h-10 items-center justify-center rounded-full border border-solid border-black/[.08] px-4 text-sm transition-colors hover:border-transparent hover:bg-[#f2f2f2] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] sm:h-12 sm:min-w-44 sm:px-5 sm:text-base"
+              className="flex h-10 items-center justify-center rounded-full border border-solid border-black/8 px-4 text-sm transition-colors hover:border-transparent hover:bg-[#f2f2f2] sm:h-12 sm:min-w-44 sm:px-5 sm:text-base dark:border-white/[.145] dark:hover:bg-[#1a1a1a]"
               href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               target="_blank"
               rel="noopener noreferrer"
